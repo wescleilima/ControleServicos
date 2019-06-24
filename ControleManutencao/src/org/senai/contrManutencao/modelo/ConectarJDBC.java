724c060615a5b07bf0ead6a7c771ede86bbdb84b
QUA209.003 <QUA209.003@LAB-F05-10.academico.local>
2019-06-24 19:35:51 -0300

    jdbc

---
+++ ControleManutencao/src/org/senai/contrManutencao/modelo/ConectarJDBC.java
@@ -0,0 +1,22 @@
+package org.senai.contrManutencao.modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConectarJDBC {
+
+	public Connection getConectar() {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection("jdbc:mysql://localhost/controle_manutencao", "root", "");
+
+		} catch (Exception e) {
+			System.out.println("erro de conexão");
+			return null;
+		}
+
+	}
+
+}
+
